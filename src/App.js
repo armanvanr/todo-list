936056e98498c898cfa5f355763fb2da558de0c7
Venska Arman Nur Rosyidin <armanvenska@gmail.com>
2022-10-27 07:00:42 +0700

    Update App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
-import { useState } from 'react';
-import './App.css';
+import React from "react";
+import TodoList from "./pages/TodoList";
 
-const Square = ({ title }) => {
-  return (
-    <div className='square-style'>
-      {title}
-    </div>
-  );
-};
-
-const App = () => {
-  const [todos, setTodos] = useState([
-    { id: 1, title: 'run 10km' },
-    { id: 2, title: 'eat chicken' },
-  ]);
-return (
-  <div>
-    <h1>To Do List</h1>
-
-    <input />
-    <button>Add</button>
-
-    <div className='list-container'>
-      {todos.map((todo) => {
-        return (
-          <Square
-            title={todo.title}
-            key={`todo-${todo.id}`} />)
-      })}
-    </div>
-  </div>
-);
-
-};
+function App() {
+  return <TodoList />;
+}
 
 export default App;
